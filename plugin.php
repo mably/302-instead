86a95d27584c82dc3c3769a1682f0a4ca1abce3e
Tim Crockford <tim@crockford.id.au>
2014-05-27 16:30:38 +0700

    Modified 302-plugin to also return a 301 if the URL happens to be a short url. Used to permanently redirect old short codes to new ones.

--- plugin.php
+++ plugin.php
@@ -1,19 +1,39 @@
 <?php
-  /*
-Plugin Name: 302 Instead
-Plugin URI: https://github.com/EpicPilgrim/302-instead
-Description: Send a 302 (temporary) redirect instead of 301 (permanent) for sites where shortlinks may change
-Version: 1.2
-Author: BrettR
-Author URI: https://github.com/EpicPilgrim/302-instead
-  */
 
-yourls_add_action('pre_redirect','temp_instead_function');
-function temp_instead_function($args) {
-  $url  = $args[0];
-  $code = $args[1];
-  if ($code != 302) {
-    // Redirect with 302 instead
-    yourls_redirect($url,302);
-  }
-}
+    /*
+     Plugin Name: 302 Instead + 301 for YOURLS URLs
+     Plugin URI: https://github.com/EpicPilgrim/302-instead
+     Description: Send a 302 (temporary) redirects that do not redirect to other short URLs and a 301 for YOURLS URLs
+     Version: 1.2
+     Author: BrettR / Tim Crockford
+     Author URI: https://github.com/timcrockford/302-instead
+     */
+
+    yourls_add_action('pre_redirect', 'temp_instead_function');
+
+    // This function will check the URL and the HTTP status code of the passed
+    // in arguments. If the URL happens to be an existing short URL on the same
+    // YOURLS installation, it does nothing. Otherwise it will send a 302
+    // redirect. Useful when you want to change the short URLs that end users
+    // might be using but you can't change.
+    
+    function temp_instead_function($args) {
+        $url   = $args[0];
+        $code  = $args[1];
+        $match = strpos($url, yourls_site_url());
+        
+        // We check here if the url contains the YOURLS installation address,
+        // and if it doesn't we'll return a 302 redirect if it isn't getting
+        // one already.
+        if ( $code != 302 && $match === false ) {
+            yourls_redirect($url, 302);
+        }
+
+        // We check here if the url contains the YOURLS installation address,
+        // and if it does we'll return a 301 redirect if it isn't getting
+        // one already.
+        if ( $code != 301 && $match !== false ) {
+            yourls_redirect($url, 301);
+        }
+    }
+?>
